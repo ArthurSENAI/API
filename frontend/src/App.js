c73e5060f61a5234ac0d37d8061887f738f75345
ArthurSENAI <arthur.s.araujo7@aluno.senai.br>
2024-05-22 14:53:35 -0300

    corrigindo erros frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import ListItems from './components/ListItems';
 import AddItem from './components/AddItem';
 import EditItem from './components/EditItem';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <h1>API Frontend</h1>
-        <Switch>
-          <Route exact path="/" component={ListItems} />
-          <Route path="/add" component={AddItem} />
-          <Route path="/edit/:id" component={EditItem} />
-        </Switch>
-      </div>
-    </Router>
+    <div>
+      <h1>API Frontend</h1>
+      <Routes>
+        <Route path="/" element={<ListItems />} />
+        <Route path="/add" element={<AddItem />} />
+        <Route path="/edit/:id" element={<EditItem />} />
+      </Routes>
+    </div>
   );
 }
 
